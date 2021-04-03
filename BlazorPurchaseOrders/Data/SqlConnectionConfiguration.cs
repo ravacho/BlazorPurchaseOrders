31e941f85641f693c9b52d775cf3d6b84df1da1d
Miguel Angel Seijas Carballo <mseijasc@yahoo.es>
2021-04-03 16:59:59 +0200

    Agregar archivos de proyecto.

---
+++ BlazorPurchaseOrders/Data/SqlConnectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPurchaseOrders.Data
+{
+    public class SqlConnectionConfiguration
+    {
+        public SqlConnectionConfiguration(string value) => Value = value;
+        public string Value { get; }
+    }
+}
